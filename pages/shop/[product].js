77eada8f5e9c80124fce86941bfe42e258c2d55b
Yevhen <Mokaron4eg@gmail.com>
2022-08-15 22:57:16 +0300

    add desktop media query

--- pages/shop/[product].js
+++ pages/shop/[product].js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { HiArrowNarrowLeft } from 'react-icons/hi';
 
@@ -13,16 +13,16 @@ const SingleProduct = () => {
 
     const router = useRouter();
 
-    const takeProduct = async(id) => {
+    const takeProduct = async (id) => {
         try {
-        const res = await takeOneProduct(id);
+            const res = await takeOneProduct(id);
 
-        if (res) {
-            setProduct(res)
-        }
+            if (res) {
+                setProduct(res);
+            }
         } catch (err) {
-            console.log(err)
-        }        
+            console.log(err);
+        }
     };
 
     useEffect(() => {
@@ -33,7 +33,7 @@ const SingleProduct = () => {
         if (productId) {
             takeProduct(productId);
         }
-        return
+        return;
     }, [productId]);
 
     return product ? (
@@ -46,34 +46,37 @@ const SingleProduct = () => {
             </div>
             <div className={styles.infoWrapper}>
                 <div className={styles.mainInfo}>
-                    <div className={styles.imgWrapper}>
+                    <div className={styles.imageWrapper}>
                         {product.imgUrl && (
-                            <Image 
+                            <Image
                                 src={product.imgUrl}
                                 alt={product.name}
-                                width={product.width ?`${product.width}px` : '300px'}
+                                width={product.width ? `${product.width}px` : '300px'}
                                 height={product.height ? `${product.height}px` : '240px'}
                             />
                         )}
                     </div>
                     <div className={styles.info}>
-                        <div><b>{product.name}</b></div>
+                        <div>
+                            <b>{product.name}</b>
+                        </div>
                         <div>{product.color}</div>
                         <div>${product.price}</div>
-                        <div>{product.quantity >= 50 
-                            ? 'Available' : product.quantity <= 50 > 0 
-                            ? 'Product is running out' : 'Sold'
-                        }</div>
+                        <div>
+                            {product.quantity >= 50
+                                ? 'Available'
+                                : product.quantity <= 50 > 0
+                                ? 'Product is running out'
+                                : 'Sold'}
+                        </div>
                     </div>
                 </div>
-                <div className={styles.description}>
-                    {product?.description}
-                </div>
+                <div className={styles.description}>{product?.description}</div>
             </div>
         </div>
     ) : (
         <div>Somthing wrong</div>
-    )
+    );
 };
 
 export default SingleProduct;
